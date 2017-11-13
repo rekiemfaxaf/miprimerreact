ed395a77cbb337bc5750ad03e4af473d678b63f2
Felipe Valdivia <pipe.faxaf@gmail.com>
2017-11-12 21:53:51 -0300

    mi primer componente

--- src/App.js
+++ src/App.js
@@ -2,19 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Title from './components/Title';
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      numero: 0
+    };
+
+    this.updateNumero = this.updateNumero.bind(this);
+  }
+
+  updateNumero(){
+    this.setState({
+      numero: this.state.numero + 1
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      return (
+        <section>
+        <div>
+          <div>
+            <Title></Title>
+            <h3>{this.state.numero}</h3>
+            <button onClick={()=>{this.updateNumero()}} >Crear cuenta gratuita</button>
+            <img src={process.env.PUBLIC_URL + 'images/title.png'} height="300" />
+            <div>
+              <ul>
+                <li>
+                  <h3>Calicaciones con emociones</h3>
+                  <p>Calica tus lugares con experencias, no con números</p>
+                </li>
+                <li>
+                  <h3>¿Sin Internet? Sin probmeas</h3>
+                  <p>PLaces funciona sin internet o en conexiones lentas</p>
+                </li>
+                <li>
+                  <h3>Tus lugares favoritos</h3>
+                  <p>define tu lista de sitions favoritos</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        </section>
+      );
   }
 }
 
