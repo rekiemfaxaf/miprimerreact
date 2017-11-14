ae2d5afe2a062223a77b592a64bafa7e3321208b
Felipe Valdivia <pipe.faxaf@gmail.com>
2017-11-14 16:27:38 -0300

    yarn - flexbox - title

--- src/App.js
+++ src/App.js
@@ -1,56 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton'
+import {indigo400, redA400, lightBlueA400, amberA400} from 'material-ui/styles/colors'
+import {Card, CardText} from 'material-ui/Card'
 import './App.css';
 
 import Title from './components/Title';
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-
-    this.state = {
-      numero: 0
-    };
-
-    this.updateNumero = this.updateNumero.bind(this);
-  }
-
-  updateNumero(){
-    this.setState({
-      numero: this.state.numero + 1
-    });
-  }
-
   render() {
       return (
-        <section>
-        <div>
-          <div>
-            <Title></Title>
-            <h3>{this.state.numero}</h3>
-            <button onClick={()=>{this.updateNumero()}} >Crear cuenta gratuita</button>
-            <img src={process.env.PUBLIC_URL + 'images/title.png'} height="300" />
-            <div>
-              <ul>
-                <li>
+      <MuiThemeProvider>
+      <div className="Header-background">
+        <div className="Header-main">
+          <Title></Title>
+          <RaisedButton label= "Crear cuenta gratuita" secondary={true}/>
+          <img className="Header-ilustration" src={process.env.PUBLIC_URL + 'images/title.png'} height="300" />
+        </div>
+        <div >
+          <ul>
+            <Card className="Header-Benefit">
+              <CardText>
+              <div className="row">
+                <div className="Header-Benefit-image" style={{'backgroundColor': redA400}}>
+                  <img src={process.env.PUBLIC_URL + 'images/title.png'} />
+                </div>
+                <div className="Header-Benefit-content">
                   <h3>Calicaciones con emociones</h3>
                   <p>Calica tus lugares con experencias, no con números</p>
-                </li>
-                <li>
+                </div>
+              </div>
+              </CardText>
+            </Card>
+
+            <Card className="Header-Benefit">
+              <CardText>
+              <div className="row">
+                <div className="Header-Benefit-image" style={{'backgroundColor': lightBlueA400}}>
+                  <img src={process.env.PUBLIC_URL + 'images/title.png'} />
+                </div>
+                <div className="Header-Benefit-content">
                   <h3>¿Sin Internet? Sin probmeas</h3>
                   <p>PLaces funciona sin internet o en conexiones lentas</p>
-                </li>
-                <li>
-                  <h3>Tus lugares favoritos</h3>
-                  <p>define tu lista de sitions favoritos</p>
-                </li>
-              </ul>
-            </div>
-          </div>
+                </div>
+              </div>
+              </CardText>
+            </Card>
+
+            <Card className="Header-Benefit">
+              <CardText>
+              <div className="row">
+                <div className="Header-Benefit-image" style={{'backgroundColor': amberA400}}>
+                  <img src={process.env.PUBLIC_URL + 'images/title.png'} />
+                </div>
+                <div className="Header-Benefit-content">
+                <h3>Tus lugares favoritos</h3>
+                <p>define tu lista de sitions favoritos</p>
+                </div>
+              </div>
+              </CardText>
+            </Card>
+          </ul>
         </div>
-        </section>
-      );
+
+      </div>
+      <div style={{'backgroundColor':indigo400, 'padding': '50px'}}>
+      </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
